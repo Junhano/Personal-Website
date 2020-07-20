3346bbdfeca354e318786423d11a82f74ea4aa8b
root <junhano@uci.edu>
2020-07-20 00:28:18 +0000

    Initial Commit for personal website

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express'),
+	  app     = express()
+
+
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + "/public"))
+
+
+
+
+app.get('/', (req, res) =>{
+	res.render('home')
+})
+
+app.get('/game', (req, res)=>{
+	res.render('game')
+})
+
+app.get('/AI', (req, res)=>{
+	res.render('AI')
+})
+
+app.get('/pproject', (req, res)=>{
+	res.render('personalproject')
+})
+
+app.get('/music', (req, res)=>{
+	res.render('music')
+})
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server Has Started!");
+});
